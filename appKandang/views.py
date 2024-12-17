cc497697b8ba5de2bdef0dd8622d704c69d83160
Seven4T <kamalmt22d@student.unhas.ac.id>
2024-12-17 22:17:41 +0800

    manambah detail kandang dan hapus kandang

--- appKandang/views.py
+++ appKandang/views.py
@@ -11,6 +11,24 @@ def index(request):
         kandang.is_penuh = kandang.ayam.count() >= kandang.kapasitas
     return render(request, 'index.html', {'kandang_list': kandang_list})
 
+def detail_kandang(request, kandang_id):
+    kandang = get_object_or_404(KandangAyam, id=kandang_id)
+    list_ayam = Ayam.objects.filter(kandang=kandang)
+
+    jenis = request.GET.get('jenis')
+    if jenis:
+        list_ayam = list_ayam.filter(jenis=jenis)
+
+    usia_min = request.GET.get('usia_min')
+    usia_max = request.GET.get('usia_max')
+
+    if usia_min:
+        list_ayam = list_ayam.filter(usia__gte=int(usia_min))
+    if usia_max:
+        list_ayam = list_ayam.filter(usia__lte=int(usia_max))
+
+    return render(request, 'pages/daftarAyam.html', {'list_ayam': list_ayam, 'kandang': kandang})
+
 def tambah_edit_kandang(request, kandang_id=None):
     if kandang_id:
         kandang = get_object_or_404(KandangAyam, id=kandang_id)
@@ -39,10 +57,12 @@ def tambah_ayam(request):
         jenis_ayam = request.POST.get('jenisAyam')
         usia = request.POST.get('usia')
         kandang_id = request.POST.get('kandangAyam')
+        jumlah = int(request.POST.get('jumlah', 1))
 
         kandang = KandangAyam.objects.get(id=kandang_id)
-        ayam = Ayam(jenis=jenis_ayam, usia=usia, kandang=kandang)
-        ayam.save()
+        for _ in range(jumlah):
+            ayam = Ayam(jenis=jenis_ayam, usia=usia, kandang=kandang)
+            ayam.save()
 
         return redirect('index')
     
@@ -56,3 +76,9 @@ def hapus_kandang(request, kandang_id):
         kandang.delete()
 
         return redirect('index')
+
+def hapus_ayam(request, ayam_id):
+    ayam = get_object_or_404(Ayam, id=ayam_id)
+    kandang_id = ayam.kandang.id
+    ayam.delete()
+    return redirect('detail_kandang', kandang_id=kandang_id)
