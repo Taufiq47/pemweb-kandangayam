bd02204764e2249712b4f03a10a82f822352c243
Seven4T <kamalmt22d@student.unhas.ac.id>
2024-12-17 22:19:54 +0800

    menambahkan path untuk detail halaman dan hapus ayam

--- appKandang/urls.py
+++ appKandang/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('tambah-kandang/', views.tambah_edit_kandang, name='tambah_kandang'),
     path('tambah-ayam/', views.tambah_ayam, name='tambah_ayam'),
+    path('detail-kandang/<int:kandang_id>', views.detail_kandang, name='detail_kandang'),
     path('edit-kandang/<int:kandang_id>/', views.tambah_edit_kandang, name='edit_kandang'),
     path('hapus-kandang/<int:kandang_id>/', views.hapus_kandang, name='hapus_kandang'),
+    path('hapus-ayam/<int:ayam_id>/', views.hapus_ayam, name='hapus_ayam'),
+
 ]
